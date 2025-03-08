0961b673cb2cfe4c8d53d6534a22f75d3440859d
Steve <lysoll8444@gmail.com>
2025-03-08 17:02:47 -0600

    Rename File & Added Scraper Class

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from scrapeClass import AmazonScraper, EbayScraper
+from Scraper import AmazonScraper, EbayScraper
 
 def main():
 	amazon_example = AmazonScraper("https://www.amazon.com/dp/B09HWYBCXQ/ref=sspa_dk_detail_2?psc=1&pd_rd_i=B09HWYBCXQ&pd_rd_w=eysxy&content-id=amzn1.sym.386c274b-4bfe-4421-9052-a1a56db557ab&pf_rd_p=386c274b-4bfe-4421-9052-a1a56db557ab&pf_rd_r=1JBF395G0Y4J0Z1Z7ZK9&pd_rd_wg=QTt2R&pd_rd_r=caee3cc8-e913-473d-97e2-6deaf2f1c00d&s=kitchen&sp_csd=d2lkZ2V0TmFtZT1zcF9kZXRhaWxfdGhlbWF0aWM")
