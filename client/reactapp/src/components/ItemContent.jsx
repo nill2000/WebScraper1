9860d62b9e2995b53a721d8d6287b2ff7c46e0c1
nill2000 <lysoll8444@gmail.com>
2025-04-06 18:02:34 -0500

    Changed DB Structure and Visibility in Dashboard.jsx

--- client/reactapp/src/components/ItemContent.jsx
+++ client/reactapp/src/components/ItemContent.jsx
@@ -3,17 +3,31 @@ function ItemContent({productName, productPrice, productLink}){
 
 	const [visible, setVisible] = useState(true);
 
-	if(!visible){
-		return null;
-	}
+	
+
+    const handleDelete = async () => {
+        // Removes from frontend
+        setVisible(false);
+
+        // const response = await fetch("http://127.0.0.1:8000/product/{item_id}", {
+        //     // Send information to backend
+		// 	method: "DELETE", 
+        //     // Tells what kind of data - sending json data, not raw text or anything else
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	},
+            
+		// 	body: JSON.stringify({ item_id: item_id }),
+		// });
+    }
 
 	return(
-		<div className="ItemContainer">
+		visible ? (<div className="ItemContainer">
 			<div className="FirstItem">
 				<span className="">{productName}</span>
 				<button 
 					className="DeleteBtn absolute right-5" 
-					onClick={() => setVisible(false)}>
+                    onClick={() => handleDelete()}>
 					Delete
 				</button>
 			</div>
@@ -24,7 +38,8 @@ function ItemContent({productName, productPrice, productLink}){
 			</a>
 			<p className="self-center">Price: {productPrice}</p>
 		</div>
-	)
+	): null
+)
 }
 
 export default ItemContent;
--- client/reactapp/src/components/ProtectedRoute.jsx
+++ client/reactapp/src/components/ProtectedRoute.jsx
@@ -3,6 +3,7 @@ import { Navigate } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../Firebase'; // adjust if your path is different
 
+// Function checks if user is loading or logged in; Redirects to necessary page
 function ProtectedRoute({ children }) {
 	const [user, loading] = useAuthState(auth);
 
