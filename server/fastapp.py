e1ee8a1b5592e2d949857a6156d1341ece5b9bbd
Steve <lysoll8444@gmail.com>
2025-04-12 18:38:31 -0500

    Saving uid into mongodb

--- server/fastapp.py
+++ server/fastapp.py
@@ -24,12 +24,14 @@ def scrape(req: ScrapeRequest):
     url = req.url                   #Req is the json sent from frontend. so req.url getting the .url object from frontend
     uid = req.uid
     data = scrape_url(url)          #Scrapes for the data
-    data = product_to_db(data)             #Saves data into database
     
     # After getting the mongodb json, add the uid key-value pair
     data["uid"] = uid
+    data = product_to_db(data)             #Saves data into database
     
-    return {"title": data["title"], "price": data["price"], "link": data["link"], "id": data["_id"]}
+    
+    
+    return {"title": data["title"], "price": data["price"], "link": data["link"], "id": data["_id"], "uid": data["uid"]}
 
 # Simplified approach compared to sending a json object
 @app.delete("/product_delete/{productId}")
